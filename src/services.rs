39fc9016275c0d48ffae148a71793fdad71439e9
Benedikt Hollerauer <benedikt.hollerauer@braintower.de>
2025-01-20 11:57:51 +0100

    fixed async problem

--- src/services.rs
+++ src/services.rs
@@ -4,7 +4,7 @@ use crate::models::{Order, StockData, Stock};
 use crate::config::config;
 
 pub trait TradingApiService {
-    fn get_stock_data(stock_id: Stock) -> Result<StockData, AppErrors>;
+    async fn get_stock_data(stock_id: Stock) -> Result<StockData, AppErrors>;
     fn place_order(order: Order) -> Result<bool, AppErrors>;
 }
 
@@ -17,9 +17,10 @@ pub trait AiService {
 pub struct AiServiceLive;
 
 impl TradingApiService for TradingApiServiceLive {
-    fn get_stock_data(stock: Stock) -> Result<StockData, AppErrors> {
+    async fn get_stock_data(stock: Stock) -> Result<StockData, AppErrors> {
         let api_key = alpha_vantage::set_api(config.alpha_vantage_api_key, reqwest::Client::new());
-        let test = api_key.stock_time(StockFunction::Monthly, &stock.get_ticker_symbol());
+        let binding = stock.get_ticker_symbol();
+        let test = api_key.stock_time(StockFunction::Monthly, &binding);
         println!("{:?}", test.json().await.unwrap());
         Ok(
             StockData {test: "".to_string()}
--- src/services_test.rs
+++ src/services_test.rs
@@ -4,21 +4,22 @@ mod TradingApiService {
     use crate::services::TradingApiServiceLive;
     use crate::services::TradingApiService;
     use crate::models::{Money, Order, OrderType, StockData, Stock};
+    use tokio::test;
 
     #[test]
-    fn test_get_stock_data_method_success() {
-        let maybe_stock_data: Result<StockData, AppErrors> = TradingApiServiceLive::get_stock_data(Stock::new("GOOG".to_string()));
+    async fn test_get_stock_data_method_success() {
+        let maybe_stock_data: Result<StockData, AppErrors> = TradingApiServiceLive::get_stock_data(Stock::new("GOOG".to_string())).await;
         assert!(maybe_stock_data.is_ok())
     }
 
     #[test]
-    fn test_get_stock_data_method_failure() {
-        let maybe_stock_data: Result<StockData, AppErrors> = TradingApiServiceLive::get_stock_data(Stock::new("Not a stock".to_string()));
+    async fn test_get_stock_data_method_failure() {
+        let maybe_stock_data: Result<StockData, AppErrors> = TradingApiServiceLive::get_stock_data(Stock::new("Not a stock".to_string())).await;
         assert!(maybe_stock_data.is_err())
     }
 
     #[test]
-    fn test_place_order_method_success() {
+    async fn test_place_order_method_success() {
         let order_success_mock: Order = Order {
             order_id: "1".to_string(),
             stock_id: Stock::new("GOOG".to_string()),
@@ -31,7 +32,7 @@ mod TradingApiService {
     }
 
     #[test]
-    fn test_place_order_method_failure() {
+    async fn test_place_order_method_failure() {
         let order_failure_mock: Order = Order {
             order_id:  "1".to_string(),
             stock_id: Stock::new("GOOG".to_string()),
